7bd6e24c1a462152eb00a8e11e6025db982a1eef
Haoran Feng <haoran.feng@alo7.com>
2016-11-28 17:26:50 +0800

    manage state and props

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -3,12 +3,18 @@ import React from "react";
 export default class Title extends React.Component {
   constructor() {
     super();
-    this.name = "hfeng";
+    this.state = {
+      title: "Welcome",
+    };
   }
 
   render() {
+
+    setTimeout(() => {
+      this.setState({title: Math.random()});
+    }, 1000);
     return (
-        <h1> {this.props.title} </h1>
+        <h1> {this.state.title} </h1>
     );
   }
 }
