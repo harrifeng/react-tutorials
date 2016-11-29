7a84fba3fc3b5e66d0f4ca6840795a19cb4ead89
Haoran Feng <haoran.feng@alo7.com>
2016-11-29 17:36:29 +0800

    Router is just a facebook shipped component, get some function to pass into

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./components/Layout";
 
-ReactDOM.render(<Layout />,
-                document.getElementById('app'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout} >
+    </Route>
+  </Router>,
+  document.getElementById('app'));
