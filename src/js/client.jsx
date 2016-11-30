af1bc7876adb93b4324cda971d67b302976b6f88
Haoran Feng <haoran.feng@alo7.com>
2016-11-30 14:27:11 +0800

    state from this link https://facebook.github.io/react/docs/state-and-lifecycle.html

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Archives from "./pages/Archives"
-import Featured from "./pages/Featured"
-import Layout from "./pages/Layout"
-import Settings from "./pages/Settings"
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
 
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout} >
-      <IndexRoute component={Featured}></IndexRoute>
-      <Route path="archives" component={Archives}></Route>
-      <Route path="settings" component={Settings}></Route>
-    </Route>
-  </Router>,
-  document.getElementById('app'));
+setInterval(tick, 1000);
