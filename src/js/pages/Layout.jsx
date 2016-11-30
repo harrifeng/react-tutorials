3efcd7b25e117e495bce24976efcc2998374cc36
Haoran Feng <haoran.feng@alo7.com>
2016-11-30 12:38:08 +0800

    konw more about the onClick

--- src/js/pages/Layout.jsx
+++ src/js/pages/Layout.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Link } from "react-router";
 
 export default class Layout extends React.Component {
+  navigate() {
+    console.log(this.props);
+  }
 
   render() {
     return (
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        <Link to="archives"> archives</Link>
+        <Link to="archives" class="btn btn-danger"> archives</Link>
         <Link to="settings"> <button class="btn btn-success">settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>featured</button>
       </div>
     );
   }
