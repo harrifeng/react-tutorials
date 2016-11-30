e9d834f2db967727f7d7ef00a02e603438438067
Haoran Feng <haoran.feng@alo7.com>
2016-11-30 14:30:53 +0800

    distract the component as a class

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
+
+}
+
 function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
   ReactDOM.render(
-    element,
+    <Clock />,
     document.getElementById('root')
   );
 }
