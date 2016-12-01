8069c324fa45ccb30f133236208b63ae310a16ac
Haoran Feng <haoran.feng@alo7.com>
2016-12-01 10:07:20 +0800

    Great example to demonstrate following: - no state inside, use the pure function component is enough, even we   need some action, the action can also be sent from `props` - we use variable to store the element (Component is something like a   Class, while element is the instance)

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -18,11 +18,60 @@ function Greeting(props) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button = null;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <div>
-    <Greeting isLoggedIn={false} />
-    <Greeting isLoggedIn={true} />
-  </div>
-  ,
+  <LoginControl />,
   document.getElementById('root')
 );
