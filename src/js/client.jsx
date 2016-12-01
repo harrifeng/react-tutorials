40174d0a6fa476f713b3cca3af1ab05acea357cc
Haoran Feng <haoran.feng@alo7.com>
2016-12-01 09:34:15 +0800

    Revert "delete bind(), Alternative One: use property initializer syntax"
    This reverts commit bffac5d2bd01fcfb554e5a7a447b5da5c44e62eb.

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -5,10 +5,11 @@ class Toggle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {isToggleOn: true};
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  // property initializer syntax
-  handleClick = () => {
+  handleClick() {
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
