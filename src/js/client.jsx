5df5cb4e630c20f7ff4b4a773cdba23fbcd0a10f
Haoran Feng <haoran.feng@alo7.com>
2016-12-02 11:27:25 +0800

    react way to render multiple component

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -1,46 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
-  return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
-
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {showWarning: true}
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    // For setState it's also possible to pass a function with the signature
-    // function(state, props) => newState
-    this.setState(prevState => ({
-      showWarning: !prevState.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn ={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    )
-  }
-}
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
 
 ReactDOM.render(
-  <Page />,
+  <ul>
+    {listItems}
+  </ul>,
   document.getElementById('root')
 );
