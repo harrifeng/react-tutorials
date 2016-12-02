6e9d43ea62033a3537c664ac0712e06fb32737e3
Haoran Feng <haoran.feng@alo7.com>
2016-12-02 12:29:28 +0800

    when several components need to reflect the same chaning data, we can lift the shared state up to their closest common ancestor.

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -1,45 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class NameForm extends React.Component {
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p> The water would boil. </p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: 'coconut'
-    };
-
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { value: ''};
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDetault();
+  handleChange(e) {
+    this.setState({value: e.target.value});
   }
 
   render() {
+    const value = this.state.value;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pick your favorite La Croix flavor:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">GrapeFruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <fieldset>
+        <legend>Enter temperature in Celsius: </legend>
+        <input
+            value={value}
+            onChange={this.handleChange} />
+        <BoilingVerdict
+            celsius={parseFloat(value)} />
+      </fieldset>
     );
   }
 }
 
-
 ReactDOM.render(
-  <NameForm />,
+  <Calculator />,
   document.getElementById('root')
 );
