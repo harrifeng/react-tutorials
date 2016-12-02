d5aa44d51369aa2d216058eb567896617df62db7
Haoran Feng <haoran.feng@alo7.com>
2016-12-02 11:29:49 +0800

    Usually you would render lists inside a component

--- src/js/client.jsx
+++ src/js/client.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
+function NumberList(props) {
+
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  )
+}
 
 ReactDOM.render(
-  <ul>
-    {listItems}
-  </ul>,
+  <NumberList numbers={[1, 2, 3, 4, 5]} />,
   document.getElementById('root')
 );
